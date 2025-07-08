913ea8d7927a0e9b5b4488da8fb9ea67a4aa1771
Manogna Jampala <manognajampala14@gmail.com>
2025-07-08 19:57:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let scrolled = false;
-window.addEventListener('scroll', function () {
-  if (!scrolled && window.scrollY > 100) {
-    scrolled = true;
-    window.location.href = 'about.html';
-  }
-});
